011084e57b94861a5c2e8d34bc94a0befac3c680
Kevin Zhang <kevinzhang@Mac.wlan>
2025-06-14 00:31:31 -0400

    Main Content of Captain Cards

--- src/components/Officer.jsx
+++ src/components/Officer.jsx
@@ -1,5 +1,33 @@
-export default function Officer() {
+export default function Officer(props) {
+    const { name, major, year, quote, imgSrc } = props;
+
     return (
-        <></>
+        <div className="bg-white rounded-xl shadow-lg
+            transition duration-300 hover:scale-105 hover:shadow-xl"
+        >
+            <div className="p-8 grid gap-8 lg:gap-4 grid-flow-col lg:grid-flow-row 
+                justify-start lg:justify-center"
+            >
+                <div className="w-30 md:w-40 lg:w-50 justify-center">
+                    <img
+                        className="object-cover aspect-square
+                        rounded-full"
+                        src={imgSrc}
+                        alt={name}
+                    />
+                </div>
+
+                <div className="flex flex-col justify-center
+                    text-left lg:text-center *:block"
+                >
+                    <p className="text-xl font-bold">{name}</p>
+                    <p className="text-base">{major}</p>
+                    <p className="text-base text-[#a8b2a1]">{year}</p>
+                </div>
+            </div>
+
+            <hr className="text-base text-[#a8b2a1] leading-0" />
+            <p className="p-8 text-base text-center">{quote}</p>
+        </div>
     )
 }
--- src/pages/Officers.jsx
+++ src/pages/Officers.jsx
@@ -1,7 +1,34 @@
-import Officer from '../components/Officer'
+import Officer from "../components/Officer";
 
 export default function Officers() {
-    return (
-        <></>
-    )
+
+  // Expandable list of officers in case the number of them ever increases
+  const officersInfo = [
+    {
+      name: "Brennen Le",
+      major: "Computer Science & Mathematics",
+      year: "Junior",
+      quote: "If he were to be eaten by a food, it would be flan.",
+      imgSrc: "mountains-placeholder.jpg",
+    },
+    {
+      name: "Liz Frisbie",
+      major: "Biochemistry",
+      year: "Junior",
+      quote: "If she were to be eaten by a food, it would be ???.",
+      imgSrc: "mountains-placeholder.jpg",
+    },
+  ];
+
+  return (
+    <div className="py-16 h-screen relative">
+      {/* Slant divider */}
+      <h2 className="mb-16 text-3xl text-center font-bold">Meet the Captains</h2>
+      <div className="mx-4 sm:mx-auto sm:w-100 md:w-110 lg:w-200 grid lg:grid-flow-col gap-16">
+        {officersInfo.map((info, index) => (
+          <Officer key={`officer-${info.name}-${index}`} {...info} />
+        ))}
+      </div>
+    </div>
+  );
 }
