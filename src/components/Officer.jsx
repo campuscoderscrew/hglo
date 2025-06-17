6fd306b5909d7b3c8ede5196ebbdc5bbc724e3ed
Kevin Zhang <kevinzhang@Mac.wlan>
2025-06-16 23:49:36 -0400

    Tilted Divider

--- src/components/Officer.jsx
+++ src/components/Officer.jsx
@@ -5,16 +5,17 @@ export default function Officer(props) {
         <>
             {/* Card background */}
             <div className="relative before:absolute 
-                before:-translate-3 before:-inset-1 before:-z-1
-                before:bg-[#66ab7b] before:rounded-xl before:shadow-lg 
-                transition duration-300 hover:scale-105 -rotate-1 hover:-rotate-2 
-
-                *:transition-rotate *:duration-300 *:rotate-1
-                hover:*:rotate-4
-            ">
+                before:-translate-3 before:-inset-1 before:-z-1 -rotate-4
+                before:bg-[#66AB7B] before:rounded-xl before:shadow-lg 
+                
+                transition duration-300 hover:scale-105  
+                hover:-rotate-5 hover:*:rotate-6"
+            >
                 {/* Card content */}
                 <div className="w-70 sm:w-100 md:w-120 lg:w-75
-                    bg-white rounded-xl shadow-sm translate-3"
+                    translate-3 rotate-4
+                    bg-white rounded-xl shadow-sm 
+                    transition duration-300" 
                 >
                     <div className="p-4 grid gap-8 lg:gap-4 
                         grid-flow-col lg:grid-flow-row 
@@ -29,7 +30,7 @@ export default function Officer(props) {
                         </div>
 
                         <div className="flex flex-col justify-center
-                            text-left lg:text-center *:block"
+                            text-left lg:text-center"
                         >
                             <p className="text-md sm:text-xl font-bold">{name}</p>
                             <p className="text-sm sm:text-base">{major}</p>
@@ -37,7 +38,7 @@ export default function Officer(props) {
                         </div>
                     </div>
 
-                    <hr className="text-base text-[#a8b2a1] leading-0" />
+                    <hr className="text-base text-[#A8B2A1] leading-0" />
                     <p className="p-4 text-sm sm:text-base text-center">{quote}</p>
                 </div>
             </div>
--- src/pages/Officers.jsx
+++ src/pages/Officers.jsx
@@ -2,33 +2,68 @@ import Officer from "../components/Officer";
 
 export default function Officers() {
 
-  // Expandable list of officers in case the number of them ever increases
-  const officersInfo = [
-    {
-      name: "Brennen Le",
-      major: "Computer Science & Mathematics",
-      year: "Junior",
-      quote: "If he were to be eaten by a food, it would be flan.",
-      imgSrc: "mountains-placeholder.jpg",
-    },
-    {
-      name: "Liz Frisbie",
-      major: "Biochemistry",
-      year: "Junior",
-      quote: "If she were to be eaten by a food, it would be ???.",
-      imgSrc: "mountains-placeholder.jpg",
-    },
-  ];
+    // Expandable list of officers in case the number of them ever increases
+    const officersInfo = [
+        {
+            name: "Brennen Le",
+            major: "Computer Science & Mathematics",
+            year: "Junior",
+            quote: "If he were to be eaten by a food, it would be flan.",
+            imgSrc: "mountains-placeholder.jpg",
+        },
+        {
+            name: "Liz Frisbie",
+            major: "Biochemistry",
+            year: "Junior",
+            quote: "If she were to be eaten by a food, it would be ???.",
+            imgSrc: "gradient-waves.svg",
+        },
+    ];
 
-  return (
-    <div className="py-16 h-screen relative">
-      {/* Slant divider */}
-      <h2 className="mb-16 text-3xl text-center font-bold">Meet the Captains</h2>
-      <div className="mx-auto w-fit grid lg:grid-flow-col gap-32">
-        {officersInfo.map((info, index) => (
-          <Officer key={`officer-${info.name}-${index}`} {...info} />
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="relative bg-white">
+            {/* SVG background */}
+            <div className="absolute">
+                <img className="w-20" src="dot-array.svg" />
+                <img src="gradient-circle.svg" />
+                {/*<img src="gradient-hoop.svg" />
+                <img src="gradient-ripple.svg" />
+                <img src="gradient-waves.svg" />*/}
+            </div>
+            
+            {/* Tilt divider */}
+            <div className="absolute top-0 left-0 w-full
+                overflow-hidden leading-0
+                officers-tilted-divider"
+            >
+                <svg
+                    className="relative block
+                        width-[calc(100%+1.3px)] h-[250px]
+                        rotate-y-180"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 1200 120"
+                    preserveAspectRatio="none"
+                >
+                    <path
+                        d="M1200 120L0 16.48 0 0 1200 0 1200 120z"
+                        className="fill-[#CFE0D8]"
+                    />
+                </svg>
+
+            </div>
+
+            
+
+            {/* Captains content */}
+            <div className="pt-64 pb-48">
+                <h2 className="mb-24 text-3xl text-center font-bold">Meet the Captains</h2>
+
+                <div className="mx-auto w-fit grid lg:grid-cols-2 gap-32">
+                    {officersInfo.map((info, index) => (
+                        <Officer key={`officer-${info.name}-${index}`} {...info} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
