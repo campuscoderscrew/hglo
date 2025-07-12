f93db584dc2295876c16a7b41b26806d4f9922bc
Kevin Zhang <kevinzhang@Mac.wlan>
2025-07-12 15:57:11 -0400

    Added More Responsiveness to Event Cards

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -7,51 +7,86 @@ export default function Event({ img, title, month, day, time, location, descript
   return (
     <div className="w-full flex justify-center relative">
 
-
       {/* >= Large screens */}
-      <div className="bg-white w-full aspect-[3/4] rounded-lg relative hidden lg:block">
-        <img src={img} className="w-full rounded-lg relative" />
+      <div className="hidden lg:block 
+        bg-white w-full aspect-[3/4] rounded-lg relative"
+      >
+        <img 
+          src={img} 
+          className="w-full rounded-lg relative"
+        />
 
-        <div className="box-border p-6 flex flex-row gap-x-6 justify-start *:text-[#3E685F] relative bg-white -mt-4 rounded-lg">
-          <div className="flex flex-col items-center w-max">
-            <p className="font-bold text-xl">{month}</p>
-            <p className="font-bold text-3xl">{day}</p>
+        <div className="p-8 -mt-4
+            flex flex-row gap-x-6 justify-start relative 
+            bg-white box-border rounded-lg
+            text-base xl:text-lg text-[#3E685F]"
+        >
+          <div className="w-max flex flex-col items-center font-bold">
+            <p className="text-lg xl:text-xl">{month}</p>
+            <p className="text-2xl xl:text-3xl">{day}</p>
           </div>
-          <div>
-            <h2 className="text-3xl mb-4">{title}</h2>
-            <span className="flex items-center gap-x-2 mb-1">
-              <img src="/events-icons/time.svg" className="w-6" />
-              {time}
-            </span>
-            <span className="flex items-center gap-x-2 mb-4">
-              <img src="/events-icons/location.svg" className=" w-6" />
-              {location}
-            </span>
-            <p className="my-2">{description}</p>
-            <button className="bg-[#66AB7B] rounded text-white bold px-6 py-2 cursor-pointer shadow mt-4 text-left">
-              Register
-            </button>
+
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl xl:text-3xl">{title}</h2>
+
+            <div className="flex flex-col gap-1
+              *:flex *:items-center *gap-x-2"
+            >
+              <span>
+                <img src="/events-icons/time.svg" className="w-6" />
+                <span>{time}</span>
+              </span>
+
+              <span>
+                <img src="/events-icons/location.svg" className=" w-6" />
+                <span>{location}</span>
+              </span>
+            </div>
+
+            <p className="text-sm xl:text-base">{description}</p>
+            <div>
+              <button className="mt-4 px-6 py-2 bg-hglo-main-500 rounded shadow-lg
+                text-white text-left bold cursor-pointer"
+              >
+                Register
+              </button>
+            </div>
           </div>
         </div>
       </div>
 
 
       {/* <= Md screens */}
-      <div className="lg:hidden w-full md:w-[90%] *:text-sm *:md:text-xl aspect-[7/3] sm:aspect-[7/2] min-h-max flex bg-white relative sm:px-4 rounded-lg p-1 sm:p-4 box-border">
+      <div className="lg:hidden w-full md:w-[90%] xs:aspect-[7/2]
+        min-h-max p-4
+        flex relative bg-white rounded-lg box-border 
+        text-xs sm:text-sm md:text-base"
+      >
+        <div className="self-center h-20 xs:h-full aspect-square">
+          <img 
+            src={img} 
+            className="size-full object-cover
+              relative rounded-lg m-auto" 
+          />
+        </div>
 
-        <img src={img} className="h-[95%] sm:h-[80%] aspect-square object-cover rounded-lg relative m-auto" />
-
-        <div className="w-full box-border p-6 flex flex-col *:text-[#3E685F] relative rounded-lg justify-center">
+        <div className="p-4 w-full
+            flex flex-col justify-center relative 
+            box-border rounded-lg text-[#3E685F]"
+        >
           <div className="mb-2 w-full flex justify-between">
             <span className="font-bold">{month} {day}</span>
             <span>{time}</span>
           </div>
-          <div>
-            <h2 className="text-xl md:text-2xl mb-4">{title}</h2>
-            <span className="sm:flex items-center gap-x-2 hidden">
-              <img src="/events-icons/location.svg" className=" w-6" />
-              {location}
-            </span>
+
+          <h2 className="mb-4 text-base sm:text-lg md:text-xl">{title}</h2>
+
+          <div className="flex items-center gap-x-2">
+            <img 
+              src="/events-icons/location.svg" 
+              className="block w-6" 
+            />
+            <span>{location}</span>
           </div>
         </div>
       </div>
